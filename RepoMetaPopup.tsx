c5f0f18c9fb56dab281dcae8dee2c19b52e7661d
Navdeep Saini <navdeep@mailbox.org>
2024-10-24 22:44:20 +0530

    fix svg imports, repometapopup props

--- RepoMetaPopup.tsx
+++ RepoMetaPopup.tsx
@@ -1,36 +1,81 @@
-import Box from "@mui/material/Box"
-import CommitIcon from "data-text:./svg/Commit.svg"
-import ForkIcon from "data-text:./svg/Fork.svg"
-import GHIcon from "data-text:./svg/GH.svg"
-import IssueIcon from "data-text:./svg/Issue.svg"
-import PRIcon from "data-text:./svg/PR.svg"
-import StarIcon from "data-text:./svg/Star.svg"
+import type { PlasmoGetStyle } from "plasmo"
 import React from "react"
+import CommitIcon from "react:./svg/Commit.svg"
+import ForkIcon from "react:./svg/Fork.svg"
+import GHIcon from "react:./svg/GH.svg"
+import IssueIcon from "react:./svg/Issue.svg"
+import PRIcon from "react:./svg/PR.svg"
+import StarIcon from "react:./svg/Star.svg"
 
-//TODO: styles
-export const RepoMetaPopup = (
-  repoIdStr: string,
-  lastCommitTimestamp: string,
-  stargazerCount: number,
-  description: string,
-  prCount: number,
-  issueCount: number,
-  forkCount: number
-) => {
+export const getStyle: PlasmoGetStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = `
+    #popup-root {
+      display: flex;
+      border-radius: 20px;
+      background-color: black;
+      color: white;
+    }
+    .row {
+    }
+    .col {
+    }
+`
+  return style
+}
+
+export const RepoMetaPopup = ({
+  repoIdStr,
+  lastCommitTimestamp,
+  stargazerCount,
+  description,
+  prCount,
+  issueCount,
+  forkCount
+}) => {
   return (
-    <Box>
-      <GHIcon />
-      {repoIdStr}
-      {description}
-      <StarIcon /> {String(stargazerCount)}
-      <PRIcon />
-      {String(prCount)}
-      <IssueIcon />
-      {String(issueCount)}
-      <ForkIcon />
-      {String(forkCount)}
-      <CommitIcon />
-      {lastCommitTimestamp}
-    </Box>
+    <>
+      <div id="popup-root">
+        <div className="row">
+          <div className="col">
+            <GHIcon />
+          </div>
+          <div className="col">{repoIdStr}</div>
+        </div>
+        <div className="row">
+          <div className="col">{description}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <StarIcon />
+          </div>
+          <div className="col">{stargazerCount}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <PRIcon />
+          </div>
+          <div className="col">{prCount}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <IssueIcon />
+          </div>
+          <div className="col">{issueCount}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <ForkIcon />
+          </div>
+          <div className="col">{forkCount}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <CommitIcon />
+          </div>
+          <div className="col">{lastCommitTimestamp}</div>
+        </div>
+      </div>
+    </>
   )
 }
