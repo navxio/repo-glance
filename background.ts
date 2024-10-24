1a5b81cb27e30e2dfb872aeafb50c0ac337496b0
Navdeep Saini <navdeep@mailbox.org>
2024-10-25 00:45:27 +0530

    add refresh repo cache listener to bg

--- background.ts
+++ background.ts
@@ -1,11 +1,5 @@
-import { useAtom } from "jotai"
-
-import { hoverDelayAtom } from "~storageAtoms"
-import { authenticateWithGitHub } from "~util"
-
-const SetupInstall = () => {
-  const [_] = useAtom(hoverDelayAtom)
-}
+import { authenticateWithGitHub, fetchRepoDetails } from "~gh.util"
+import storage from "~storage"
 
 /* EVENT LISTENERS */
 chrome.runtime.onInstalled.addListener((details) => {
@@ -13,5 +7,28 @@ chrome.runtime.onInstalled.addListener((details) => {
     console.log("Extension installed")
 
     authenticateWithGitHub()
+  }
+})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "REFRESH_REPO_CACHE") {
+    const { owner, name } = message
+
+    // Fetch latest data from GitHub
+    fetchRepoDetails({ owner, name })
+      .then((newData) => {
+        // Update localStorage with fresh data and new timestamp
+        return storage.set(owner + "-" + name, newData)
+        // Optionally, send response back to content script
+      })
+      .then((_) => {
+        sendResponse({ success: true })
+        // Return true to keep the message channel open
+        return true
+      })
+      .catch((e) => {
+        console.error(`Error refreshing repo data ${owner + "/" + name} `)
+        sendResponse({ success: false })
+      })
   }
 })
