258251bc7550cfd189fb6042fcad5d51a7df3824
Navdeep Saini <navdeep@mailbox.org>
2024-10-25 01:19:08 +0530

    clean up content component to only add listeners

--- content.tsx
+++ content.tsx
@@ -1,50 +1,38 @@
-import Box from "@mui/material/Box"
-import { useAtom } from "jotai"
 import React, { useEffect, useState } from "react"
-
-import { RepoMetaPopup } from "~RepoMetaPopup"
-import { hoverDelayAtom } from "~storageAtoms"
 
 const githubRegex: RegExp =
   /https:\/\/github\.com\/(?!topics|collections|features|explore|issues|pulls|marketplace|settings|apps|events|sponsors|about|search|notifications|organizations|enterprise|stars|gists|readme|users|security|contact|solutions)([A-Za-z0-9-]+)\/([A-Za-z0-9-_]+)(\/[^?]*)?(\?.*)?$/
 
 // export this component
 export const RepoMetadataExtension = () => {
-  const [myTimeouts, setMyTimeouts] = useState([]) // will store all the timeouts
-  const [hoverDelayMS] = useAtom(hoverDelayAtom)
+  const [popup, setPopup] = useState(null)
 
   function hidePopup() {
     popup.style.visibility = "hidden"
-    myTimeouts.forEach((myTimeout) => clearTimeout(myTimeout))
+  }
+
+  async function fetchMetadataShowPopup(event, owner: string, name: string) {
+    const { default: Popup } = await import("./RepoMetaPopupUI")
+    ReactDOM.render(<Popup owner={owner} name={name} />, popup)
   }
 
   useEffect(() => {
+    const popup = document.createElement("div")
+    document.body.appendChild(popup)
+    setPopup(popup)
     // add listeners to every gh link
     document.querySelectorAll("a").forEach((link) => {
       const matches = link.href.match(githubRegex)
       if (matches && link.href.indexOf(window.location.href) === -1) {
-        const repoIdStr = matches[1] + "/" + matches[2]
-        console.info(`found gh repo ${repoIdStr}`)
-        link.addEventListener("mouseenter", (event) => {
-          const myTimeout = setTimeout(() => {
-            fetchMetadataShowPopup(event, repoIdStr)
-          }, hoverDelayMS)
-          setMyTimeouts([...myTimeouts, myTimeout])
+        const owner = matches[1]
+        const name = matches[2]
+        link.addEventListener("mouseenter", async (event) => {
+          await fetchMetadataShowPopup(event, owner, name)
         })
         link.addEventListener("mouseleave", hidePopup)
       }
     })
   }, [])
 
-  return (
-    <RepoMetaPopup
-      repoIdStr="facebook/react"
-      lastCommitTimestamp="2 hours ago"
-      stargazerCount={6666}
-      prCount={10}
-      issueCount={44}
-      forkCount={1200}
-      description={"A javascript library for building user interfaces"}
-    />
-  )
+  return <></>
 }
