a29ca1a9a88d5edd6073c88fa3af75b380445476
Navdeep Saini <navdeep@mailbox.org>
2024-10-25 01:18:54 +0530

    wrap up popupui component

--- RepoMetaPopupUI.tsx
+++ RepoMetaPopupUI.tsx
@@ -1,5 +1,5 @@
 import type { PlasmoGetStyle } from "plasmo"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import CommitIcon from "react:./svg/Commit.svg"
 import ForkIcon from "react:./svg/Fork.svg"
 import GHIcon from "react:./svg/GH.svg"
@@ -7,7 +7,10 @@ import IssueIcon from "react:./svg/Issue.svg"
 import PRIcon from "react:./svg/PR.svg"
 import StarIcon from "react:./svg/Star.svg"
 
-export const getStyle: PlasmoGetStyle = () => {
+import { fetchRepoDetails } from "~gh.util"
+import storage from "~storage"
+
+const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement("style")
   style.textContent = `
     #popup-root {
@@ -24,15 +27,38 @@ export const getStyle: PlasmoGetStyle = () => {
   return style
 }
 
-export const RepoMetaPopupUI = ({
-  repoIdStr,
-  lastCommitTimestamp,
-  stargazerCount,
-  description,
-  prCount,
-  issueCount,
-  forkCount
-}) => {
+async function getRepoDetails(owner: string, name: string) {
+  // tries the cache for ze repo and fetches it from api if not available
+  const dataKey = `${owner}-${name}`
+  const expiryKey = `${owner}-${name}-expiry`
+  const expiryTimestamp = await storage.get(expiryKey)
+  if (Date.now() > expiryTimestamp) {
+    const repoData = await fetchRepoDetails({ owner, name })
+    await storage.set(dataKey, repoData)
+    await storage.set(expiryKey, Date.now() + 3600 * 1000)
+    return repoData
+  } else {
+    const repoData = await storage.get(dataKey)
+    chrome.runtime.sendMessage({ type: "REFRESH_REPO_CACHE", owner, name })
+    return repoData
+  }
+}
+
+const RepoMetaPopupUI = ({ owner, name }) => {
+  const [repoData, setRepoData] = useState({})
+
+  useEffect(() => {
+    getRepoDetails(owner, name)
+      .then((data) => setRepoData(data))
+      .catch((e) => {
+        console.error(`Error getting repo details ${e}`)
+      })
+  }, [])
+
+  if (!repoData) {
+    return <div>Loading...</div>
+  }
+
   return (
     <>
       <div id="popup-root">
@@ -40,42 +66,44 @@ export const RepoMetaPopupUI = ({
           <div className="col">
             <GHIcon />
           </div>
-          <div className="col">{repoIdStr}</div>
+          <div className="col">{owner + "/" + name}</div>
         </div>
         <div className="row">
-          <div className="col">{description}</div>
+          <div className="col">{repoData.description}</div>
         </div>
         <div className="row">
           <div className="col">
             <StarIcon />
           </div>
-          <div className="col">{stargazerCount}</div>
+          <div className="col">{repoData.stargazerCount}</div>
         </div>
         <div className="row">
           <div className="col">
             <PRIcon />
           </div>
-          <div className="col">{prCount}</div>
+          <div className="col">{repoData.prCount}</div>
         </div>
         <div className="row">
           <div className="col">
             <IssueIcon />
           </div>
-          <div className="col">{issueCount}</div>
+          <div className="col">{repoData.issueCount}</div>
         </div>
         <div className="row">
           <div className="col">
             <ForkIcon />
           </div>
-          <div className="col">{forkCount}</div>
+          <div className="col">{repoData.forkCount}</div>
         </div>
         <div className="row">
           <div className="col">
             <CommitIcon />
           </div>
-          <div className="col">{lastCommitTimestamp}</div>
+          <div className="col">{repoData.lastCommitTimestamp}</div>
         </div>
       </div>
     </>
   )
 }
+
+export default RepoMetaPopupUI
