77cfa59996320dfa4ec5dcbd6e27506b5ce5d6ef
Navdeep Saini <navdeep@mailbox.org>
2024-12-13 21:10:11 +0530

    explicitly state area 'local' to storage

--- storage.ts
+++ storage.ts
@@ -1,4 +1,15 @@
 import { Storage } from "@plasmohq/storage"
 
-const storage = new Storage()
+const storage = new Storage({
+  area: 'local'
+})
+
+
+const BLACKLIST_KEY: string = "blacklist";
+storage.watch({
+  [BLACKLIST_KEY]: (newValue, oldValue) => {
+    console.log("Blacklist updated:", newValue);
+  }
+});
+
 export default storage
